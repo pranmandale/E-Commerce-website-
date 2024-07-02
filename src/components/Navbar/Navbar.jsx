1a5ff49460105f23ebfc9ebd6ab901115d4948e6
pranmandale <pranavmandale2025@gmail.com>
2024-07-02 18:42:42 +0530

    second nav bar included

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import logo from "../../assets/logo.png"
+import React, { useState, useEffect } from 'react';
+import logo from "../../assets/logo.png";
 // this is for search icon
 import { IoMdSearch } from "react-icons/io";
 // this is for cart icon
 import { FaCartShopping } from "react-icons/fa6";
+// this is for caret icon
+import { FaCaretDown } from "react-icons/fa";
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -23,6 +25,52 @@ const Navbar = () => {
       return newMode;
     });
   };
+
+  const Menu = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/#",
+    },
+    {
+      id: 2,
+      name: "Top Rated",
+      link: "/#services",
+    },
+    {
+      id: 3,
+      name: "Kids Wear",
+      link: "/#",
+    },
+    {
+      id: 4,
+      name: "Mens Wear",
+      link: "/#",
+    },
+    {
+      id: 5,
+      name: "Electronics",
+      link: "/#",
+    },
+  ];
+
+  const DropdownLinks = [
+    {
+      id: 1,
+      name: "Trending",
+      link: "/#",
+    },
+    {
+      id: 2,
+      name: "Best Selling",
+      link: "/#",
+    },
+    {
+      id: 3,
+      name: "Top Rated",
+      link: "/#",
+    },
+  ];
 
   return (
     <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
@@ -84,11 +132,50 @@ const Navbar = () => {
         </div>
       </div>
       {/* LowerNavbar */}
-      <div>
+      <div className="flex justify-center">
         {/* Add content for the lower navbar here */}
+        <ul className="sm:flex hidden items-center gap-4">
+          {Menu.map((data) => (
+            <li key={data.id}>
+              <a href={data.link}
+              className="inlilne-block px-4 hover:text-primary duration-200"
+              >
+              {data.name}</a>
+            </li>
+          ))}
+          {/* dropdown */}
+          <li className="group relative cursor-pointer">
+          <a
+          href="#"
+          className="flex items-center gap-[2px] py-2"
+          >
+          Trending Products
+          <span>
+            <FaCaretDown
+              className="transition-all duration-200
+              group-hover:rotate-180"
+            />
+          </span>
+          </a>
+          <div className="absolute z-[9999] hidden group-hover:block w-[150px] rounded-md bg-white
+          p-2 text-black shadow-md">
+            <ul>
+              {DropdownLinks.map((data) => (
+                <li key={data.id}>
+                  <a
+                  href= {data.link}
+                  className="inline-block w-full rounded-md p-2
+                  hover:bg-primary/20"
+                  >{data.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          </li>
+        </ul>
       </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
