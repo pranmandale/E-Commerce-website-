81002d51e89d7f7fd5ad4875779bb2d64be58796
pranmandale <pranavmandale2025@gmail.com>
2024-07-20 13:55:37 +0530

    new commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/E-Commerce-website-", // Adjust base URL as needed
+   base: "/E-Commerce-website-", // Adjust base URL as needed
+  //  base: '/',
   build: {
     // outDir: 'build', // Specify the output directory
     outDir: 'dist'
