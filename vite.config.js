64b23a1b7c7926179523157abb077064d09138ca
pranmandale <pranavmandale2025@gmail.com>
2024-07-20 16:57:07 +0530

    change in file for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  //  base: "/E-Commerce-website-", // Adjust base URL as needed
-  //  base: '/',
+     base: "/E-Commerce-website-", // Adjust base URL as needed
+    //  base: '/',
   build: {
     // outDir: 'build', // Specify the output directory
     // outDir: 'dist'
-    outDir: 'dist', 
+     outDir: 'dist', 
   },
 });
