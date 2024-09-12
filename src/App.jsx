71ffb702852d80a2e51d49bc1135064cdf7993be
pranmandale <pranavmandale2025@gmail.com>
2024-09-12 20:27:37 +0530

    minor changes in some files

--- src/App.jsx
+++ src/App.jsx
@@ -29,16 +29,6 @@ const App = () => {
         setShowAuthModal(false);
     };
 
-    // React.useEffect(() => {
-    //     AOS.init({
-    //         offset: 100,
-    //         duration: 800,
-    //         easing: 'ease-in-sine',
-    //         delay: 100,
-    //     });
-    //     AOS.refresh();
-    // }, []);
-
     return (
         <AuthProvider>
             <CartProvider>
